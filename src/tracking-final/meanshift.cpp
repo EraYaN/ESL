5a7071678f8394a29f426e8e61d3a0b968f97b34
Robin Hes <robinhes@outlook.com>
2017-05-15 17:45:13 +0200

    Now calculating all iterations using NEON.

--- src/tracking-final/meanshift.cpp
+++ src/tracking-final/meanshift.cpp
@@ -80,11 +80,20 @@ float MeanShift::weightKernel(int curr_pixel, cv::Mat &target_model, cv::Mat &ta
 }
 
 #ifdef ARMCC
+/**
+ * Run the weight calculation kernel in blocks of eight at a time using NEON.
+
+ * Ideally, we would supply target_model_row, target_candidate_row and
+ * weight_row as type float32_t*, but a gcc bug prevents us from doing so,
+ * hence the casts.
+ */
 void MeanShift::weightKernel_NEON(const uint8_t *curr_pixels, const float *target_model_row, const float *target_candidate_row, const float *weight_row)
-{  
-    // Load current pixels into a vector of 8 uint8 elements, expand into a vector of 8 uint16 elements.
+{
+    // Load current pixels into a vector of 8 uint8 elements, expand into a
+    // vector of 8 uint16 elements.
     uint16x8_t pixel_vec = vmovl_u8(vld1_u8(curr_pixels));
-    // Split the vector in two, expand both high and low parts to uint32 vectors and convert to float.
+    // Split the vector in two, expand both high and low parts to uint32
+    // vectors and convert to float.
     float32x4_t pixel_vec_high = vcvtq_f32_u32(vmovl_u16(vget_high_u16(pixel_vec)));
     float32x4_t pixel_vec_low = vcvtq_f32_u32(vmovl_u16(vget_low_u16(pixel_vec)));
     // Multiply both parts to obtain bins and convert back to int.
@@ -92,7 +101,8 @@ void MeanShift::weightKernel_NEON(const uint8_t *curr_pixels, const float *targe
     uint32x4_t bin_vec2 = vcvtq_u32_f32(vmulq_n_f32(pixel_vec_high, reciprocal_bin_width));
 
     float32x4_t model_vec1, model_vec2, candidate_vec1, candidate_vec2, multiplier1, multiplier2;
-    // Load elements for both sets of vectors individually, due to a lack of memory locality.
+    // Load elements for both sets of vectors individually,
+    // due to a lack of memory locality.
     model_vec1 = vdupq_n_f32(0.0f);
     model_vec1 = vld1q_lane_f32((float32_t*)&target_model_row[vgetq_lane_u32(bin_vec1, 0)], model_vec1, 0);
     model_vec1 = vld1q_lane_f32((float32_t*)&target_model_row[vgetq_lane_u32(bin_vec1, 1)], model_vec1, 1);
@@ -153,7 +163,13 @@ cv::Mat MeanShift::CalWeight(const cv::Mat &frame, cv::Mat &target_model, cv::Ma
     int row_index = rec.y;
     int col_index = rec.x;
 
+    // Initialize weight matrix width to be a multiple of 8 so we can use NEON
+    // for every iteration, while preventing memory access violations.
+#ifdef __ARM_NEON__
+    cv::Mat weight(rows, (cols / 8) * (8 + 1), CV_32F, cv::Scalar(1.0000));
+#else
     cv::Mat weight(rows, cols, CV_32F, cv::Scalar(1.0000));
+#endif
     std::vector<cv::Mat> bgr_planes;
     cv::split(frame, bgr_planes);
 
@@ -161,29 +177,16 @@ cv::Mat MeanShift::CalWeight(const cv::Mat &frame, cv::Mat &target_model, cv::Ma
         row_index = rec.y;
         for (int i = 0; i < rows; i++) {
             col_index = rec.x;
-#ifdef ARMCC
-            int neon_iterations = (rec.width / 8) * 8;
-
-            // Run the weight calculation kernel in blocks of eight at a time using NEON.
-            for (int j = 0; j < neon_iterations; j += 8) {
+#ifdef __ARM_NEON__
+            for (int j = 0; j < cols; j += 8) {
                 uint8_t *curr_pixels = &(bgr_planes[k].ptr<uchar>(row_index)[col_index]);
                 weightKernel_NEON(curr_pixels, target_model.ptr<float>(k), target_candidate.ptr<float>(k), &(weight.ptr<float>(i)[j]));
                 col_index += 8;
             }
-
-            // Calculate final weights (that don't fit in a block) sequentially.
-            for (int j = neon_iterations; j < cols; j++) {
-                int curr_pixel = (bgr_planes[k].at<uchar>(row_index, col_index));
-                weight.at<float>(i, j) *= weightKernel(curr_pixel, target_model, target_candidate, k);
-
-                col_index++;
-            }
-
 #else
-            // Calculate each weight sequentially.
             for (int j = 0; j < cols; j++) {
                 int curr_pixel = (bgr_planes[k].at<uchar>(row_index, col_index));
-                weight.at<float>(i,j) *= weightKernel(curr_pixel, target_model, target_candidate, k);
+                weight.at<float>(i, j) *= weightKernel(curr_pixel, target_model, target_candidate, k);
 
                 col_index++;
             }
@@ -213,8 +216,8 @@ cv::Rect MeanShift::track(const cv::Mat &next_frame)
         next_rect.width = target_Region.width;
         next_rect.height = target_Region.height;
 
-        for (int i = 0; i < weight.rows; i++) {
-            for (int j = 0; j < weight.cols; j++) {
+        for (int i = 0; i < target_Region.height; i++) {
+            for (int j = 0; j < target_Region.width; j++) {
                 float norm_i = static_cast<float>(i - centre) / centre;
                 float norm_j = static_cast<float>(j - centre) / centre;
                 mult = pow(norm_i, 2) + pow(norm_j, 2) > 1.0 ? 0.0 : 1.0;
@@ -229,7 +232,8 @@ cv::Rect MeanShift::track(const cv::Mat &next_frame)
 
         if (abs(next_rect.x - target_Region.x) < 1 && abs(next_rect.y - target_Region.y) < 1) {
             break;
-        } else {
+        }
+        else {
             target_Region.x = next_rect.x;
             target_Region.y = next_rect.y;
         }
