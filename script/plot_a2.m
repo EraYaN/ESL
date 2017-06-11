c8b75dce4874325fef3725c5b63e3ea5ee8cb1a2
Erwin de Haan <EraYaN@users.noreply.github.com>
2017-06-11 17:24:32 +0200

    Final version.

--- script/plot_a2.m
+++ script/plot_a2.m
@@ -1,5 +1,5 @@
-c = {'Naive', 'Optimised', 'Optimised (fixp.)', 'NEON', 'DSP', 'DSP + Optimised', 'DSP + NEON'};
-speedups = [1 1; 14.3 4.4; 0 0; 20 4.7; 0 0; 0 0; 0 0];
+c = {'Naive', 'Optimised', 'Optimised (fixp.)', 'NEON (compiler only)', 'NEON (compiler only, fixp.)', 'NEON', 'NEON (fixp.)', 'DSP', 'DSP + Optimised', 'DSP + NEON'};
+speedups = [1 1; 20.3 4.8; 31.8 5.1; 20.3 4.8; 31.9 5.1; 25.2 4.9; 48.1 5.3; 11.4 4.1; 14.0 4.3; 20.3 4.6];
 bar(speedups)
 line(xlim, [1 1], 'LineStyle', '--', 'Color', [0.2, 0.2, 0.2]);
 ax = gca;
